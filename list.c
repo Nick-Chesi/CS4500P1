8c708a792ff6322011d5b4be9a82175fea16bf61
Nick-Chesi <143734499+Nick-Chesi@users.noreply.github.com>
2024-02-14 16:50:52 -0700

    Update list.c

--- list.c
+++ list.c
@@ -1,1 +1,24 @@
+#include "list.h"
+#include <stdio.h>
+#include <string.h>
 
+list* createlist(){
+    // Allocating memory for the List
+    list* ll = (list*)malloc(sizeof(list));
+    // Could not allocate memory
+    if (ll == NULL){
+        return NULL;
+    }
+    // Setting ll head to null 
+    ll -> head = NULL;
+    // Returning the struct
+    return ll;
+}
+
+int addToList(list* ll, char* item){
+    // Allocating memory for the node
+    node* newNode = (node* )malloc(sizeof(node));
+    // Could not allocate memory for the node
+    if (){
+    }
+}
